7438fb2951cdaa757433d18163b4906c74575424
FBI1337 <FBI13377@yandex.ru>
2024-10-07 16:07:28 +0300

    fix

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-const PORT = process.env.PORT || 3001
+const app = express();
+const PORT = 5000;
 
-const app = express()
+app.use(express.json());
 
-app.listen(PORT, () => {
-    console.log(`Server starting on ${PORT}`)
+mongoose.connect('mongodb+srv://slavashelkynov1337:8g5GBJVcGSMFvBca@authentication.9lvzi.mongodb.net/authentication?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 })
+.then(() => console.log('MnogoDB connected'))
+.catch(err => console.error('Connection error', err));
 
-app.length('/api', (req, res) => {
-    res.json({
-        message: "Hello gnida express.js"
-    })
+const userShema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+});
+
+const User = mongoose.model('User', userShema);
+
+app.post('/api/register', async (req,res) => {
+    const { username, email, password } = req.body;
+
+    try {
+        const existingUser = await User.findOne({ email });
+        if (existingUser) return res.status(400).json({ message: 'Пользователь уже существует' });
+    }
 })
